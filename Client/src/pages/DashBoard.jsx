ac0fb939dd5e4ad78495846cdc15ff0ddb172e98
alok <alok00007pandey@gmail.com>
2025-08-06 05:03:25 +0530

    Initial commit Day1 of project

---
+++ Client/src/pages/DashBoard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DashBoard = () => {
+  return (
+    <div><h1>DashBoard</h1></div>
+  )
+}
+
+export default DashBoard
---
+++ Client/src/pages/WriteArticle.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WriteArtice = () => {
+  return (
+    <div><h1>WriteArtice</h1></div>
+  )
+}
+
+export default WriteArtice
