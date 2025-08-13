66372147f3795a64406930bc80c184a1d057a699
alok <alok00007pandey@gmail.com>
2025-08-13 16:19:07 +0530

    done till image generation page

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import AiTools from '../components/AiTools'
+import Testimonial from '../components/Testimonial'
+import Plan from '../components/Plan'
+import Footer from '../components/Footer'
+import { Layout } from 'lucide-react'
 const Home = () => {
   return (
     <>
     <Navbar/>
     <Hero/>
     <AiTools/>
+    <Testimonial/>
+    <Plan/>
+    <Footer/>
+ 
     </>
   )
 }
